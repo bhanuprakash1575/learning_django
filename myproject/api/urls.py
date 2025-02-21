cc81395f84bbda380539afb052fb5e80a5c72ad9
Bhanu <apple@APPLEs-MacBook-Pro.local>
2025-02-21 09:06:51 +0530

    implemented rest apis for post model

--- myproject/api/urls.py
+++ myproject/api/urls.py
@@ -1,6 +1,9 @@
-from .views import test
+from .views import test, PostListCreateView, PostDetailRetrieveUpdateDestroy
 from django.urls import path
 
 urlpatterns = [
     path('test/',test,name='test'),
+
+    path('post/',PostListCreateView.as_view(),name='post-list-create'),
+    path('post/<int:pk>/',PostDetailRetrieveUpdateDestroy.as_view(),name='post-detail-retrieve-update-destroy'),
 ]
--- myproject/api/views.py
+++ myproject/api/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from rest_framework import generics
+
+from blog.models import Post
+from .serializer import PostSerializer
 
 
 
 def test(request):
     return HttpResponse('Hello, World. this api test response!')
+
+class PostListCreateView(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostDetailRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
